a8dc8154d7408c6dbb906607e56eea50a857cdfc
Luis Felipe <111872373+luizinfrd@users.noreply.github.com>
2023-11-09 11:20:47 -0300

    Add files via upload

---
+++ ordemServicoRoutes.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+const express = require("express");
+const osRouter = express.Router();
+
+const validarSessao = require("../modules/validarSessao");
+
+osRouter.get("/", validarSessao, (req, res) => {
+    fetch(process.env.APIURL + `/ordem-servico`, {
+        method: "GET",
+        headers: {
+            "Content-type": "Application/JSON",
+            "codSessao": req.session.codSessao
+        }
+    })
+    .then((response) => response.json())
+    .then((response) => res.json(response))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Erro ao conectar com o servidor." });
+    });
+});
+
+osRouter.get("/:codOS", validarSessao, (req, res) => {
+    const codOS = req.params.codOS;
+
+    fetch(process.env.APIURL + `/ordem-servico/${codOS}`, {
+        method: "GET",
+        headers: {
+            "Content-type": "Application/JSON",
+            "codSessao": req.session.codSessao
+        }
+    })
+    .then((response) => response.json())
+    .then((response) => res.json(response))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Erro ao conectar com o servidor." });
+    });
+});
+
+osRouter.post("/", validarSessao, (req, res) => {
+    fetch(process.env.APIURL + `/ordem-servico`, {
+        method: "POST",
+        headers: {
+            "Content-type": "Application/JSON",
+            "codSessao": req.session.codSessao
+        },
+        body: JSON.stringify(req.body)
+    })
+    .then((response) => response.json())
+    .then((response) => res.json(response))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Erro ao conectar com o servidor." });
+    });
+});
+
+osRouter.put("/", validarSessao, (req, res) => {
+    fetch(process.env.APIURL + `/ordem-servico`, {
+        method: "PUT",
+        headers: {
+            "Content-type": "Application/JSON",
+            "codSessao": req.session.codSessao
+        },
+        body: JSON.stringify(req.body)
+    })
+    .then((response) => response.json())
+    .then((response) => res.json(response))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Erro ao conectar com o servidor." });
+    });
+});
+
+osRouter.delete("/:codOS", validarSessao, (req, res) => {
+    const codOS = req.params.codOS;
+
+    fetch(process.env.APIURL + `/ordem-servico/${codOS}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "Application/JSON",
+            "codSessao": req.session.codSessao
+        }
+    })
+    .then((response) => response.json())
+    .then((response) => res.json(response))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Erro ao conectar com o servidor." });
+    });
+});
+
+module.exports = osRouter;
---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const app = express();
+
+app.use(express.json());
+app.use(session({
+    secret: process.env.key,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false
+    }
+}));
+app.use(express.static("./public/"));
+app.use("/", require("./routes/routes"));
+app.use("/clientes", require("./routes/clientesRoutes"));
+app.use("/componentes", require("./routes/componentesRoutes"));
+app.use("/entradas", require("./routes/entradasRoutes"));
+app.use("/estoque", require("./routes/estoqueRoutes"));
+app.use("/fabricantes", require("./routes/fabricantesRoutes"));
+app.use("/funcionarios", require("./routes/funcionariosRoutes"));
+app.use("/ordem-servico", require("./routes/ordemServicoRoutes"));
+app.use("/saidas", require("./routes/saidasRoutes"));
+app.use("/sessao", require("./routes/sessaoRoutes"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server is running -> ' + port));
